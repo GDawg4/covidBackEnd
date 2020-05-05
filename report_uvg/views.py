fa223418e453c311e180bd1ca4372b201d1a0b70
Diego Sebastián González Solís <49197018+tabufellin@users.noreply.github.com>
2020-05-05 15:41:24 -0600

    report uvg endpointers

--- report_uvg/views.py
+++ report_uvg/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from report_uvg.models import Report_uvg
+from report_uvg.serializers import SelfReportSerializer
+
+# Create your views here.
+
+
+
+
+
+
+
+
+@action(detail=True, methods=['get'])
+def reports_uvg(self, request, pk=None):
+    reports_uvg = self.get_object()
+    response = []
+    for report_uvg in reports_uvg.object.all():
+        response.append(report_uvg)
+
+    return Response(response)
+    
